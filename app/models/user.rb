ad8b837a2e37b45a615255874cef1ef7db342654
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-09 14:46:50 +1100

    Fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :posts
 
-  validates :username, presence: true, unique: true
-  validates :email, presence: true, unique: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 2, maximum: 18 }
 end
