b8fe7594b873f86417e01f3e343c8d2e7249b546
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2023-12-09 13:50:46 +1100

    Add model validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  validates :user, presence: true
+  validates :body, presence: true, length: { minimum: 1, maximum: 2000 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :posts
+
+  validates :username, presence: true, unique: true
+  validates :email, presence: true, unique: true
+  validates :password, presence: true, length: { minimum: 2, maximum: 18 }
 end
